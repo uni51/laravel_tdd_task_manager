7b017528e5ec866b482f1e1bef5fe577f79304c8
uni51 <uni51@users.noreply.github.com>
2020-01-30 23:14:18 +0900

    第1章 タスクリスト表示：01.ルート設定とテストのためのコントローラ実装

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class TaskControllerTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Get All Tasks Path Test
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testGetAllTasksPath()
     {
-        $response = $this->get('/');
+        $response = $this->get('/tasks');
 
         $response->assertStatus(200);
     }
